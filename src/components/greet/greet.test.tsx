38fa907c71190f2150f79e01aac8463c99af4ea7
Shelumiel <sssharon7@gmail.com>
2022-09-02 11:49:11 +0800

    Add Grouping Tests

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './greet';
 
-test('Greet renders correctly',()=>{
-    render(<Greet/>)
-    const textElement=screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
-})
-
-test('Greet renders with a name',()=>{
-    render(<Greet name='bob'/>)
-    const textElement=screen.getByText(/hello bob/i)
-    expect(textElement).toBeInTheDocument()
+describe('Greet',()=>{
+    test('renders correctly',()=>{
+        render(<Greet/>)
+        const textElement=screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    test('renders with a name',()=>{
+        render(<Greet name='bob'/>)
+        const textElement=screen.getByText(/hello bob/i)
+        expect(textElement).toBeInTheDocument()
+    })
 })
